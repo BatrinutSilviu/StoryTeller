8691a6c63ba95b8dfdf7634f22820b32884964f4
silviu <batrinutsilviu@gmail.com>
2026-01-25 20:58:24 +0200

    add swagger

---
+++ app/api-docs/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+import dynamic from 'next/dynamic'
+import 'swagger-ui-react/swagger-ui.css'
+
+const SwaggerUI = dynamic(() => import('swagger-ui-react'), { ssr: false })
+
+export default function ApiDoc() {
+    return (
+        <section className="container">
+            <SwaggerUI url="/api/doc" />
+        </section>
+    )
+}
--- app/api/profiles/[id]/route.ts
+++ app/api/profiles/[id]/route.ts
@@ -1,6 +1,52 @@
 import { NextResponse } from 'next/server'
 import {prisma} from "@/lib/prisma";
 
+
+/**
+ * @swagger
+ * /api/profiles/{id}:
+ *   get:
+ *     summary: Gets a profile
+ *     description: Returns all info associated with a specific profile
+ *     tags:
+ *       - Profiles
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: The profile ID
+ *     responses:
+ *       200:
+ *         description: Successful response
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   id:
+ *                     type: integer
+ *                   profile_id:
+ *                     type: integer
+ *                   category_id:
+ *                     type: integer
+ *                   category:
+ *                     type: object
+ *                     properties:
+ *                       id:
+ *                         type: integer
+ *                       name:
+ *                         type: string
+ *       400:
+ *         description: Invalid profile ID
+ *       404:
+ *         description: No categories found
+ *       500:
+ *         description: Server error
+ */
 export async function GET(
     request: Request,
     { params }: { params: Promise<{ id: string }> }
